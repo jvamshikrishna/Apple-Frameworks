2081df34dc7c4e60996ff89cae4eaebaa8342397
Vamshi <jvamshikrishna1997@gmail.com>
2023-12-03 08:24:13 -0600

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Vamshi krishna Jillapally on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
